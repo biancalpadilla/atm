3d0db45bff703532f3615d7bf3695610dbb3f96e
Bianca Padilla <Bianca@LiveNinja.com>
2017-09-26 08:21:47 -0400

    refactored ATM class

--- atm.rb
+++ atm.rb
@@ -43,77 +43,97 @@ end
 class Atm
 
 	def initialize(checking_account = 0, savings_account = 0)
-		@starting_checking_balance = checking_account
-		@starting_savings_balance = savings_account
+		@checking_account = Account.new("Checking Account", checking_account)
+		@savings_account = Account.new("Savings Account", savings_account)
 	end
 
 	def run
-		checking_account = Account.new("Checking Account", @starting_checking_balance)
-		savings_account = Account.new("Savings Account", @starting_savings_balance)
 
 		while true
-			puts "--------ATM-------"
-			puts "1. Check balance"
-			puts "2. Make a Deposit"
-			puts "3. Make a Withdrawal"
-			puts "4. Make a Transfer"
-			puts "5. Logout"
-			puts "------------------"
 
-			option1 = gets.chomp.to_i
-
-			case option1
+			case atm_message()
 			when 1
-				savings_account.show_balance()
-				checking_account.show_balance()
+				show_balance()
 			when 2
-				puts "Which account would you like to deposit into?"
-				puts "1. Savings Account"
-				puts "2. Checking Account"
-				option2 = gets.chomp.to_i
-
-				puts "How much?"
-				dep_amount = gets.chomp.to_i
-
-				case option2
-				when 1
-					savings_account.deposit(dep_amount)
-				when 2
-					checking_account.deposit(dep_amount)
-				end
+				deposit()
 			when 3
-				puts "Which account would you like to withdraw from?"
-				puts "1. Savings Account"
-				puts "2. Checking Account"
-				option3 = gets.chomp.to_i
-
-				puts "How much?"
-				wit_amount = gets.chomp.to_i
-
-				case option3
-				when 1
-					savings_account.withdraw(wit_amount)
-				when 2
-					savings_account.withdraw(wit_amount)
-				end
+				withdraw()
 			when 4
-				puts "From which account would you like to transfer out of?"
-				puts "1. Savings Account"
-				puts "2. Checking Account"
-				option4 = gets.chomp.to_i
-
-				puts "How much?"
-				trans_amount = gets.chomp.to_i
-
-				case option4
-				when 1
-					savings_account.transfer(checking_account,trans_amount)
-				when 2
-					checking_account.transfer(savings_account,trans_amount)
-				end
+				transfer()
 			when 5
 				break
 			end
 		end
+
 	end
+
+	def atm_message
+		puts "--------ATM-------"
+		puts "1. Check balance"
+		puts "2. Make a Deposit"
+		puts "3. Make a Withdrawal"
+		puts "4. Make a Transfer"
+		puts "5. Logout"
+		puts "------------------"
+
+		choice = gets.chomp.to_i
+		return choice
+	end
+
+	def show_balance
+		@savings_account.show_balance()
+		@checking_account.show_balance()
+	end
+
+	def deposit
+		puts "Which account would you like to deposit into?"
+		puts "1. Savings Account"
+		puts "2. Checking Account"
+		choice = gets.chomp.to_i
+
+		puts "How much?"
+		amount = gets.chomp.to_i
+
+		case choice
+		when 1
+			@savings_account.deposit(amount)
+		when 2
+			@checking_account.deposit(amount)
+		end
+	end
+
+	def withdraw
+		puts "Which account would you like to withdraw from?"
+		puts "1. Savings Account"
+		puts "2. Checking Account"
+		choice = gets.chomp.to_i
+
+		puts "How much?"
+		amount = gets.chomp.to_i
+
+		case choice
+		when 1
+			@savings_account.withdraw(amount)
+		when 2
+			@checking_account.withdraw(amount)
+		end
+	end
+
+	def transfer
+		puts "From which account would you like to transfer out of?"
+		puts "1. Savings Account"
+		puts "2. Checking Account"
+		choice = gets.chomp.to_i
+
+		puts "How much?"
+		amount = gets.chomp.to_i
+
+		case choice
+		when 1
+			@savings_account.transfer(@checking_account, amount)
+		when 2
+			@checking_account.transfer(@savings_account, amount)
+		end
+	end
+
 end
