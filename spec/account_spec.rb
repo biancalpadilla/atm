06eb7c45c30f449c329fc13111065e3a7b662c42
Bianca Padilla <Bianca@LiveNinja.com>
2017-09-26 10:18:54 -0400

    finished account tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,7 @@ require '../lib/account'
 RSpec.describe Account do
 
     let(:account1){Account.new("Checking",1000)}
+    let(:account2){Account.new("Savings",1000)}
 
     it ".creates a new account" do
         expect(Account.new("Checking", 100)).to be_an_instance_of Account
@@ -26,6 +27,22 @@ RSpec.describe Account do
 
     it ".#amount= returns a new account amount" do
         expect(account1.amount = 1500).to eql(1500)
+    end
+
+    it "#deposits $20" do
+        account1.deposit(20)
+        expect(account1.amount).to eql(1020)
+    end
+
+    it "#withdraws $20" do
+        account1.withdraw(20)
+        expect(account1.amount).to eql(980)
+    end
+
+    it "#transfers $500 from account1 to account2" do
+        account1.transfer(account2,500)
+        expect(account1.amount).to eql(500)
+        expect(account2.amount).to eql(1500)
     end
 
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -4,9 +4,7 @@ require '../lib/atm'
 RSpec.describe Atm do
 
     it ".creates a new atm session" do
-        expect(Atm.new()).to be_an_instance_of Atm
+        expect(Atm.new).to be_an_instance_of Atm
     end
-
-
 
 end
